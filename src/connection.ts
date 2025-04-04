ea8cee68bb454db569e53ad56ca91090df9bb4f2
Savio Picanço <saviopbrito@outlook.com>
2025-04-04 14:29:32 -0300

    add new features

--- src/connection.ts
+++ src/connection.ts
@@ -1,20 +1,19 @@
 import {  makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion, DisconnectReason } from '@whiskeysockets/baileys';
 import * as path from 'path';
 import pino from 'pino';
+import qrcode from "qrcode";
 
-export const connect = async () => {
+let qrCodeData = "";
 
-    const { state, saveCreds } = await useMultiFileAuthState(
-        path.resolve(path.dirname("."), '..', "assets", "auth", "baileys")
-    );
-
+export const connect = async (app) => {
+    const { state, saveCreds } = await useMultiFileAuthState("./auth_info_baileys");
     const {version} = await fetchLatestBaileysVersion();
     
     const socket = makeWASocket({
         printQRInTerminal: true, 
         version,
         logger: pino({ level: "error" }),
-        auth: state, 
+        auth:state, 
         browser: ["Chrome (Linux)", "", ""],
         markOnlineOnConnect: true,
     });
@@ -25,20 +24,38 @@ export const connect = async () => {
 
     
     socket.ev.on("connection.update", (update) => {
-        const { connection, lastDisconnect } = update; 
+        const { connection, lastDisconnect ,  qr } = update; 
 
+        if (qr) {
+            const filePath = path.join(__dirname, "public", "qrcode.png");
+    
+          
+            qrcode.toFile(filePath, qr, (err) => {
+                if (err) {
+                    console.error("Erro ao salvar o QR Code:", err);
+                } else {
+                    console.log("QR Code salvo em:", filePath);
+                }
+            });
+
+        
+        }
+    
+    
+      
         if (connection == "close" ) {
             const shouldReconnect = 
                 lastDisconnect?.error?.cause != DisconnectReason.loggedOut;
 
             if (shouldReconnect) {
-                connect();
+                connect(app);
             }
         }
+        
+        
     });
 
-      
-   
+
 
     //socket.ev.on("messages.update", (message) => {
      //   console.log("messages.update"); 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
+import path from "path";
 import { connect } from "./connection";
 import { load } from "./load";
+import express from "express"
+import open from "open"; 
+
+const app = express();
+const PORT = 80;
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em porta ${80}`);
+    //open(`http://localhost:${PORT}`);
+    start(); 
+});
+
+
+app.get("/", (req, res) => {
+    return res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 async function start() {
-    const socket = await connect();
+    const socket = await connect(app);
     load(socket);
+    
 }
 
-start(); 
